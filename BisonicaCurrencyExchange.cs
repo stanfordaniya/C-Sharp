f85b35dc83f30cd961daaeabd0e43ccbd161198c
Aniya Stanford <91031207+stanfordaniya@users.noreply.github.com>
2023-07-23 18:07:50 -0400

    Bisonica Currency Exchange

---
+++ BisonicaCurrencyExchange.cs
@@ -0,0 +1,15 @@
+class Assignment2C
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("[Bisonica Currency Exchange]");
+
+        Console.WriteLine("General Bison welcomes you to Pax Bisonica!");
+
+        Console.WriteLine("How many US Dollars do you have?");
+      
+        int dollars = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(dollars + " US Dollar(s) is " + Math.Max(dollars/1.176, 1) + " British Pound(s), which is worth " + (dollars) + " Bison Dollar(s)! ");
+    } 
+}
