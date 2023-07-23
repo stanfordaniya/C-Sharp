50a004f55ac72c54836b51d66593273a510f63ae
Aniya Stanford <91031207+stanfordaniya@users.noreply.github.com>
2023-07-23 18:00:08 -0400

    What's The Next Three Characters?

---
+++ NextCharacter.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Assignment1C
+{
+    public static void Main(string[] args)
+    {
+      
+      Console.WriteLine("Please enter a letter: ");  
+     
+      char a = char.Parse(Console.ReadLine());
+      int i = a;
+      Console.WriteLine("The next three characters after "+ a + " are "+ (char)(i+1)+ ", "+(char)(i+2)+", "+(char)(i+3)+ ".");
+  }
+}
