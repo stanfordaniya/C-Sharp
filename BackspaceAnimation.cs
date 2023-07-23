6c239a9707573dff5a70421fc98e385c2ca37d8b
Aniya Stanford <91031207+stanfordaniya@users.noreply.github.com>
2023-07-23 18:18:48 -0400

    Backspace Animation

---
+++ BackspaceAnimation.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Assignment4C
+{
+    public static void Main(string[] args)
+    {
+        string sentence;
+        int letters;
+        Console.WriteLine("[Backspace Animation]\nPlease enter your sentence:");
+        sentence = Console.ReadLine();
+        //prompts the user for a sentence
+        Console.WriteLine("How many letters do you want to backspace?");
+        letters = Convert.ToInt32(Console.ReadLine());
+        Console.Write(sentence);
+        //if(sentence -= letters){
+        //Pause the program for 0.5 seconds
+        for (int i = 0; i < letters; i++)
+        {
+            System.Threading.Thread.Sleep(500);
+            Console.Write("\b \b");
+        }
+        //Note: You may need to use “\b \b” rather than just “\b”
+    }
+
+}
